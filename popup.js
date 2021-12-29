86dc04a18494125b96983769fbd3d5201ced4de1
Qingzhe Wu <qingzhe_wu@trendmicro.com>
2021-12-29 14:13:24 +0800

    🎉 init git

---
+++ popup.js
@@ -0,0 +1,29 @@
+$("#allToggle").click(function () {
+
+    chrome.runtime.sendMessage({
+        "cmd": "setStatus",
+        all: $("#allToggle")[0].checked
+    })
+    chrome.tabs.query({
+        currentWindow: true,
+        active: true
+    }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {
+            cmd: "checkStatus",
+            all: $("#allToggle")[0].checked
+        }, function (res) {})
+    })
+})
+
+chrome.runtime.sendMessage({
+    "cmd": "checkStatus"
+},item => {
+    if (item.all === undefined) {
+        $("#allToggle")[0].checked = true
+        chrome.runtime.sendMessage({
+            "cmd": "setStatus",
+            all: true
+        })
+    }
+    $("#allToggle")[0].checked = item.all
+})
